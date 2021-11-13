4aa679d06961aa07f6526409ec28bc1e9b2d5f1c
xsmileh <85039207+xsmileh@users.noreply.github.com>
2021-11-13 22:50:08 +0300

    Add files via upload

--- ymgk_digitalsafebox/hafta_7-1/digitalsafebox_project/digitalsafebox_project/app.py
+++ ymgk_digitalsafebox/hafta_7-1/digitalsafebox_project/digitalsafebox_project/app.py
@@ -20,12 +20,23 @@ APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Thisissupposedtobesecret!'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:\\Users\\ES\\Desktop\\database.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:\\Users\\ES\\Desktop\\database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///D:\\ymgk_digitalsafebox\\hafta_7-1\\digitalsafebox_project\\database.db'
+
+
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
+
+
+class FileContents(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(300))
+    data=db.Column(db.LargeBinary)
+
+
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -89,11 +100,6 @@ def signup():
 
 
 
-
-
-
-
-
 @app.route("/upload", methods=["POST"])
 def upload():
     '''
@@ -101,6 +107,9 @@ def upload():
     if os.path.isdir(os.path.join(APP_ROOT, 'files/{}'.format(folder_name))):
         print("folder exist")
     '''
+
+
+
     target = os.path.join(APP_ROOT, 'safebox/')
 
     print(target)
@@ -112,11 +121,7 @@ def upload():
     for upload in request.files.getlist("file"):
         print(upload)
         print("{} is the file name".format(upload.filename))
-
         filename = upload.filename
-        # cvp=str(filename_path[0]+filename_path[1])
-
-
 
       
 
@@ -131,26 +136,52 @@ def upload():
 
 
 
-        destination = "/".join([target, filename])
+        # destination = "/".join([target, filename])
+
 
 
 
         encryptor=Encryptor()
-        mykey=encryptor.key_create()
-        encryptor.key_write(mykey, 'mykey.key')
-        loaded_key=encryptor.key_load('mykey.key')
+        # mykey=encryptor.key_create()
+        key = Fernet.generate_key()
 
 
-        encryptor.file_encrypt(loaded_key,"C:\\Users\\ES\\Desktop\\"+filename, target+filename)
-        # encryptor.file_decrypt(loaded_key, 'C:\\Users\\ES\\Desktop\\enc_1111.jpg', 'C:\\Users\\ES\\Desktop\\dec_1111.jpg')
 
 
-        print("Accept incoming file:", filename)
-        print("Save it to:", destination)
 
-        # upload.save(destination)
+        #OLUŞTURULAN KEY'i DİZİNE KAYDETMEK İÇİN
+        # encryptor.key_write(mykey, str(os.path.splitext(filename)[0])+'.key')
+        # loaded_key=encryptor.key_load(str(os.path.splitext(filename)[0])+'.key')
+
+
+
+        #DOSYA ŞİFRELEME/DİZİN:safebox
+        encryptor.file_encrypt(key,"C:\\Users\\ES\\Desktop\\"+filename, target+"e-"+filename)
+
+
+        #ŞİFRELİ DOSYAYI ÇÖZME/DİZİN:safebox
+        encryptor.file_decrypt(key, target+"e-"+filename, target+"d-"+filename)
+
+
+
+
+        # print("Accept incoming file:", filename)
+        # print("Save it to:", destination)
+        # # upload.save(destination)
 
     # return send_from_directory("safebox", filename, as_attachment=True)
+
+
+        file=request.files["file"]
+        newFile=FileContents(name=file.filename,data=file.read())
+        db.session.add(newFile)
+        db.session.commit()
+
+
+
+
+
+
     return render_template("complete.html", image_name=filename)
 
 
@@ -195,25 +226,22 @@ def logout():
 
 
 
-
-
-
-
-
 class Encryptor():
 
-    def key_create(self):
-        key = Fernet.generate_key()
-        return key
 
-    def key_write(self, key, key_name):
-        with open(key_name, 'wb') as mykey:
-            mykey.write(key)
+    #KEY OLUŞTURMA ve KEY'İ KAYDETME
+    # def key_create(self):
+    #     key = Fernet.generate_key()
+    #     return key
 
-    def key_load(self, key_name):
-        with open(key_name, 'rb') as mykey:
-            key = mykey.read()
-        return key
+    # def key_write(self, key, key_name):
+    #     with open(key_name, 'wb') as mykey:
+    #         mykey.write(key)
+
+    # def key_load(self, key_name):
+    #     with open(key_name, 'rb') as mykey:
+    #         key = mykey.read()
+    #     return key
 
 
     def file_encrypt(self, key, original_file, encrypted_file):
